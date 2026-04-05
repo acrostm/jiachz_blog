5afaf01c247bf1aec5b90636f15d2bb5cfa873e6
acrostm <jzhang3615@gatech.edu>
2026-04-05 05:47:35 -0700

    chore: remove old database credentials and standardize on Vercel native environment variables

--- config/db.ts
+++ config/db.ts
@@ -1,5 +1,9 @@
 export const DATABASE_URL = process.env.DATABASE_URL;
 
+export const POSTGRES_PRISMA_URL = process.env.POSTGRES_PRISMA_URL;
+
+export const POSTGRES_URL_NON_POOLING = process.env.POSTGRES_URL_NON_POOLING;
+
 export const KV_URL = process.env.KV_URL;
 
 export const REDIS_HOST = process.env.REDIS_HOST;
